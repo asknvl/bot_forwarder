b23f1aeeb7d2f7fdc594200e5052d7ccf3b45198
Alexey Konovalov <asknvl@gmail.com>
2023-12-23 23:31:45 +0300

    init

---
+++ bot_forwarder/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace bot_forwarder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bot = new bot_forwarder("6973654807:AAFggHHa5zEGO5aZnWzJZw6TAmEEasQNJeY");
+            bot.start();
+
+
+            string text = "";
+            do
+            {
+                text = Console.ReadLine();
+            } while (!text.Equals("quit"));
+
+        }
+    }
+}
---
+++ bot_forwarder/bot_forwarder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace bot_forwarder
+{
+    public class bot_forwarder
+    {
+
+        #region const
+        long OUT_CHANNEL_ID = -1002085060438;
+        #endregion
+
+        #region vars
+        string Token;
+        ITelegramBotClient bot;
+        CancellationToken cancellationToken;
+        #endregion
+
+        public bot_forwarder(string token) { 
+
+            bot = new TelegramBotClient(token);
+
+        }
+
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken cancellationToken) { 
+        
+            switch (update.Type)
+            {
+
+                case UpdateType.ChannelPost:
+                    try
+                    {
+                        var m = update.ChannelPost;
+                        if (m != null)
+                        {
+
+                            await bot.ForwardMessageAsync(OUT_CHANNEL_ID, m.Chat.Id, m.MessageId);
+                            
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+
+                case UpdateType.Message:
+
+                    
+
+                    break;
+            }
+        
+        }
+
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken) { 
+            return Task.CompletedTask;
+        }
+
+        public void start() {
+
+            cancellationToken = new CancellationToken();
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = new UpdateType[] { UpdateType.Message, UpdateType.ChannelPost }
+            };
+            bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, new CancellationToken());
+        }
+
+    }    
+}
